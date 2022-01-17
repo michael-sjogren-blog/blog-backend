684aef78522f322f9e3b27466c6674737ef858c8
michael <carl.michael.sjogren@gmail.com>
2022-01-17 20:54:11 +0100

    Enable cors

--- Blog.API/Startup.cs
+++ Blog.API/Startup.cs
@@ -26,6 +26,7 @@ namespace Blog.API
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddCors();
             services.AddControllers();
             services.AddSwaggerGen(c => { c.SwaggerDoc("v1", new OpenApiInfo {Title = "Blog.API", Version = "v1"}); });
         }
@@ -39,11 +40,15 @@ namespace Blog.API
                 app.UseSwagger();
                 app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "Blog.API v1"));
             }
-
+    
             app.UseHttpsRedirection();
-
+            
             app.UseRouting();
-
+            app.UseCors(x => x
+                .AllowAnyMethod()
+                .AllowAnyHeader()
+                .SetIsOriginAllowed(origin => true) // allow any origin
+                .AllowCredentials()); // allow credentials
             app.UseAuthorization();
 
             app.UseEndpoints(endpoints => { endpoints.MapControllers(); });
