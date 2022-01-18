00fb8cf5f74c59e68fe23a010e11badce076173f
michael <carl.michael.sjogren@gmail.com>
2022-01-18 22:46:35 +0100

    user model

--- Blog.Data/Models/User.cs
+++ Blog.Data/Models/User.cs
@@ -1,8 +1,16 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace Blog.Data.Models
 {
     public class User
     {
+        [Key]
         public int Id { get; set; }
-
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Email { get; set; }
+        public string UserName { get; set; }
+        public string PhoneNumber { get; set; }
+        public string Password { get; set; }
     }
 }
