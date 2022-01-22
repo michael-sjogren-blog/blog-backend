4a7677a3e5ee7f338d96ecaba80d14327e092f22
michael <carl.michael.sjogren@gmail.com>
2022-01-22 18:58:24 +0100

    removed datatype annotation

--- Blog.Data/Transfer/Create/PostCreateDto.cs
+++ Blog.Data/Transfer/Create/PostCreateDto.cs
@@ -5,15 +5,10 @@ namespace Blog.Data.Transfer
 {
     public class PostCreateDto
     {
-        [Required]
         [MinLength(1 , ErrorMessage = "Title was empty")]
-        [DataType(DataType.Text)]
         public string? Title { get; set; }
-        [Required]
         [MinLength(1 , ErrorMessage = "Content was empty")]
-        [DataType(DataType.MultilineText)]
         public string? Content { get; set; }
-        [Required]
         [Range(1,int.MaxValue)]
         public int AuthorId { get; set; }
 
